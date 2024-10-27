419cb7dc323f4e0cfbdd4715834c90716d665253
julib111 <julibustamante75@gmail.com>
2024-10-26 23:17:37 -0500

    Commit con interfaces

--- Marketplace/src/main/java/co/edu/uniquindio/marketplace/viewcontroller/EstadisticasViewController.java
+++ Marketplace/src/main/java/co/edu/uniquindio/marketplace/viewcontroller/EstadisticasViewController.java
@@ -1,4 +1,26 @@
 package co.edu.uniquindio.marketplace.viewcontroller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+
 public class EstadisticasViewController {
+
+    @FXML
+    private PieChart pieChart;
+
+    public void initialize() {
+        cargarDatosPieChart();
+    }
+
+    private void cargarDatosPieChart() {
+        ObservableList<PieChart.Data> datos = FXCollections.observableArrayList(
+                new PieChart.Data("Categoría 1", 45),
+                new PieChart.Data("Categoría 2", 35),
+                new PieChart.Data("Categoría 3", 20)
+        );
+
+        pieChart.setData(datos);
+    }
 }
